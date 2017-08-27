bd7d5f043b2471e50a8109acf00cc34d03ad02b7
Ramsey Nasser <ram@nas.sr>
2017-08-26 22:51:15 -0400

    Make lein compatible

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject nasser/magic "0.0.1-SNAPSHOT"
+  :description "Morgan And Grand Iron Clojure"
+  :url "http://nas.sr/magic/"
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"
+            :distribution :repo}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [nasser/mage "0.0.2-SNAPSHOT"]
+                 [org.clojure/tools.analyzer "0.6.9"]])
