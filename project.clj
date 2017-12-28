73531fc229c9a8226b96eddb31ce13c9f7fdb146
Ramsey Nasser <ram@nas.sr>
2017-12-28 12:51:28 +0200

    Add references to project.clj
    Includes required *.dll files

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"
             :distribution :repo}
+  :resource-paths ["references"]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [nasser/mage "0.0.2-SNAPSHOT"]
                  [org.clojure/tools.analyzer "0.6.9"]])
