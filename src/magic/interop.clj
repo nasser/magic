c6339c8649235e6ee31a2becbcae19f62f3ec403
Ramsey Nasser <ram@nas.sr>
2020-07-10 20:13:03 -0400

    Memoize interop lookup
    Speeds up parts of the compilation

--- src/magic/interop.clj
+++ src/magic/interop.clj
@@ -10,18 +10,19 @@
            (every? #(= (first %) (last %))
                    (map vector parameter-types params))))))
 
-(defn method
-  ([t name & params]
-   (try
-     (let [methods (.GetMethods t)
-           name-matches (filter #(= name (.Name %)) methods)
-           parameter-matches 
-           (filter (parameters-match params) name-matches)]
-       (case (count parameter-matches)
-         1 (first parameter-matches)
-         0 nil
-         (throw (System.Reflection.AmbiguousMatchException.
-                 (str t "::" name " " (string/join "," params) " " (vec parameter-matches)))))))))
+(def method
+  (memoize
+   (fn [t name & params]
+     (try
+       (let [methods (.GetMethods t)
+             name-matches (filter #(= name (.Name %)) methods)
+             parameter-matches
+             (filter (parameters-match params) name-matches)]
+         (case (count parameter-matches)
+           1 (first parameter-matches)
+           0 nil
+           (throw (System.Reflection.AmbiguousMatchException.
+                   (str t "::" name " " (string/join "," params) " " (vec parameter-matches))))))))))
 
 (defn parameters
   [method] (.GetParameters method))
@@ -30,17 +31,21 @@
   [method] (map #(.ParameterType %)
                 (parameters method)))
 
-(defn field
-  ([type name] (.GetField type name)))
+(def field
+  (memoize
+   (fn [type name] (.GetField type name))))
 
-(defn constructor
-  ([type & params] (.GetConstructor type (into-array Type params))))
+(def constructor
+  (memoize
+   (fn [type & params] (.GetConstructor type (into-array Type params)))))
 
-(defn getter [type name]
-  (method type (str "get_" name)))
+(def getter
+  (memoize
+   (fn [type name] (method type (str "get_" name)))))
 
-(defn setter [type name]
-  (method type (str "set_" name)))
+(def setter
+  (memoize
+   (fn [type name] (method type (str "set_" name)))))
 
 (defn generic-type [name params]
   (when-let [base-type (clojure.lang.RT/classForName (str name "`" (count params)))]
