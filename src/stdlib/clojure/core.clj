44809475dd011001fb8234c2c7e7c67f00e1abe3
Ramsey Nasser <ram@nas.sr>
2020-07-28 15:31:45 -0400

    Remove type hint invalidated by MAGIC's semantics

--- src/stdlib/clojure/core.clj
+++ src/stdlib/clojure/core.clj
@@ -4266,7 +4266,7 @@ Note that read can execute code (controlled by *read-eval*),
    :static true}
   [ns]
   (let [ns (the-ns ns)]
-    (filter-key val (fn [^clojure.lang.Var v] (and (instance? clojure.lang.Var v)
+    (filter-key val (fn [v] (and (instance? clojure.lang.Var v)
                                  (not= ns (.ns v))))
                 (ns-map ns))))
 
