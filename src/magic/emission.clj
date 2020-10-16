2f75195958c6bb48bc58e9f5d27500521380b943
Ramsey Nasser <ram@nas.sr>
2020-10-16 12:09:02 -0400

    Patch magic.emission for .NET Standard support

--- src/magic/emission.clj
+++ src/magic/emission.clj
@@ -2,6 +2,18 @@
   (:import [System.Reflection AssemblyName]
            [System.Reflection.Emit AssemblyBuilderAccess]))
 
+;; this differs between .NET Framework and .NET Standard/Core
+(def assemby-builder-access-values (-> AssemblyBuilderAccess Enum/GetNames set))
+
+;; we use RunAndSave if its available (meaning we're on .NET Framework)
+;; and fall back to Run otherwise (meaning we're on .NET Standard/Core)
+;; we avoid emitting a static reference to either to that AOT'd assemblies
+;; can work in either context
+(def assemby-builder-access 
+  (Enum/Parse AssemblyBuilderAccess
+              (or (assemby-builder-access-values "RunAndSave")
+                  (assemby-builder-access-values "Run"))))
+
 (defn fresh-module 
   "Creates a fresh ModuleBuilder, suitable for binding to *module*"
   [name]
@@ -9,7 +21,7 @@
    (.. AppDomain CurrentDomain
        (DefineDynamicAssembly
         (AssemblyName. name)
-        AssemblyBuilderAccess/RunAndSave))
+        assemby-builder-access))
    (.DefineDynamicModule (str name ".dll"))))
 
 (def ^:dynamic
