aedb92331b6c4e2111af4819b3b5d30985749391
Ramsey Nasser <ram@nas.sr>
2022-11-25 20:34:40 -0500

    Update build script

--- build.clj
+++ build.clj
@@ -11,43 +11,79 @@
 ;; causes its own problems.
 (defmacro -forward-declare-new-vars [vars]
   `(do
-      ~@(map (fn [v] `(let [vv# (declare ~v)] (println "forward declaring" vv#))) vars)))
+     ~@(map (fn [v] `(let [vv# (declare ~v)] (println "forward declaring" vv#))) vars)))
 
 (-forward-declare-new-vars [requiring-resolve])
 
 (in-ns 'build)
 
 (def std-libs-to-compile
-  '[clojure.string ;; respect this order to prevent compiling twice the same file
-    clojure.walk
-    clojure.edn
+  '[clojure.tools.analyzer.env
+    clojure.tools.analyzer.utils
+    clojure.tools.analyzer
+    clojure.string
     clojure.set
-    clojure.spec.alpha
-    clojure.core.specs.alpha
-    clojure.main
-    clojure.pprint
-    clojure.clr.io
-    clojure.clr.shell
-    clojure.core.protocols
-    clojure.core.reducers
-    clojure.core.server
-    clojure.datafy
-    clojure.data
-    clojure.instant
-    clojure.repl
-    clojure.template
-    clojure.stacktrace
-    clojure.test
-    clojure.uuid
-    clojure.zip
+    clojure.tools.analyzer.ast
+    magic.analyzer.binder
+    magic.analyzer.util
+    magic.analyzer.reflection
+    magic.flags
+    magic.emission
+    magic.analyzer.types
+    magic.analyzer.generated-types
+    magic.analyzer.loop-bindings
+    magic.analyzer.uniquify
+    magic.interop
+    mage.core
+    magic.util
+    magic.core
+    magic.spells.lift-vars
+    magic.spells.lift-keywords
+    magic.analyzer.errors
+    magic.analyzer.analyze-host-forms
+    magic.analyzer.novel
+    magic.analyzer.intrinsics
+    magic.analyzer.typed-passes
+    clojure.tools.analyzer.passes
+    clojure.tools.analyzer.passes.source-info
+    clojure.tools.analyzer.passes.elide-meta
+    clojure.tools.analyzer.passes.trim
+    clojure.tools.analyzer.passes.cleanup
+    magic.analyzer.collect-closed-overs
+    magic.analyzer.remove-local-children
+    magic.analyzer.untyped-passes
+    clojure.walk
+    magic.analyzer
+    magic.analyzer.literal-reinterpretation
+    magic.intrinsics
     magic.api
+    clojure.core-proxy
+    clojure.core-print
+    clojure.genclass
+    clojure.core-deftype
+    clojure.core.protocols
+    clojure.gvec
+    clojure.clr.io
     clojure.core]) ;; if clojure.core not at the end, prevent other files from being compiled
+
+(defn bootstrap-portable [& opts]
+  (let [opts (set opts)]
+    (binding [*print-meta*               true
+              clojure.core/*loaded-libs* (ref (sorted-set))
+              *spells*                   [sparse-case] ; (if (:portable opts) (conj *spells* sparse-case) *spells*)
+              *eval-form-fn*             magic.api/eval
+              *compile-file-fn*          magic.api/runtime-compile-file
+              *load-file-fn*             magic.api/runtime-load-file
+              *warn-on-reflection*       true
+              *compile-path*             "bootstrap"]
+      (doseq [lib std-libs-to-compile]
+        (println (str "building " lib))
+        (compile-namespace lib {:write-files true :suppress-print-forms true})))))
 
 (defn bootstrap [& opts]
   (let [opts (set opts)]
     (binding [*print-meta*               true
               clojure.core/*loaded-libs* (ref (sorted-set))
-              *spells*                   (if (:portable opts) (conj *spells* sparse-case) *spells*)
               *eval-form-fn*             magic.api/eval
               *compile-file-fn*          magic.api/runtime-compile-file
               *load-file-fn*             magic.api/runtime-load-file
